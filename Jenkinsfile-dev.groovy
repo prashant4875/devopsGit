dd15b92f651cb7fdaa4c5e0b1ecdb3bf18d8f284
Prashant <bhadoria998@gmail.com>
2023-11-08 19:06:28 +0530

    add

--- Jenkinsfile-dev.groovy
+++ Jenkinsfile-dev.groovy
@@ -3,6 +3,14 @@ properties([
     parameters([
         choice(name: 'Environment', choices: ['dev', 'sit2'], description: 'The target environment'),
         choice(name: 'GithubRepo', choices: ['https://github.com/prashant4875/Netflix-clone.git'], description: 'Select Github repo to checkout')
+        gitParameter(
+            name: 'Branch',
+            branchFilter: "origin/develop",
+            defaultValue: 'master',
+            sortMode: 'DESCENDING_SMART',
+            type: 'PT_BRANCH',
+            useRepository: 'https://github.com/prashant4875/Netflix-clone.git'
+        )
     ])
 ])
 dev()
