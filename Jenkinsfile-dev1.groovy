9794e772593196ec05bb6b101423edd35478551a
Prashant <bhadoria998@gmail.com>
2023-11-09 18:44:46 +0530

    add

--- Jenkinsfile-dev1.groovy
+++ Jenkinsfile-dev1.groovy
@@ -3,14 +3,14 @@ properties([
     parameters([
         choice(name: 'Environment', choices: ['dev', 'sit2'], description: 'The target environment'),
         choice(name: 'GithubRepo', choices: ['https://github.com/prashant4875/two-tier-flask-app.git'], description: 'Select Github repo to checkout'),
-        // gitParameter(
-        //     name: 'Branch',
-        //     branchFilter: "origin/develop|origin/release.*",
-        //     defaultValue: 'master',
-        //     sortMode: 'DESCENDING_SMART',
-        //     type: 'PT_BRANCH',
-        //     useRepository: 'https://github.com/prashant4875/two-tier-flask-app.git'
-        // )
+        gitParameter(
+            name: 'Branch',
+            branchFilter: "origin/develop|origin/release.*",
+            defaultValue: 'master',
+            sortMode: 'DESCENDING_SMART',
+            type: 'PT_BRANCH',
+            useRepository: 'https://github.com/prashant4875/two-tier-flask-app.git'
+        )
     ])
 ])
 dev()
