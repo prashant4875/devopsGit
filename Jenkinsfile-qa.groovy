df96125d79b23de2e0c540922c868bd12e251bfd
Prashant <bhadoria998@gmail.com>
2023-12-04 16:58:21 +0530

    add

---
+++ Jenkinsfile-qa.groovy
@@ -0,0 +1,32 @@
+properties([
+    parameters([
+        choice(name: 'Environment', choices: ['dev', 'sit2'], description: 'The target environment'),
+        choice(name: 'GithubRepo', choices: ['https://github.com/prashant4875/Netflix-clone'], description: 'Select Github repo to checkout'),
+        gitParameter(
+            name: 'Branch',
+            branchFilter: "*",
+            defaultValue: 'origin/master',
+            sortMode: 'DESCENDING_SMART',
+            type: 'PT_BRANCH',
+            useRepository: 'https://github.com/prashant4875/Netflix-clone.git'
+        )
+    ])
+])
+pipeline{
+    agent any
+
+    stages{
+        stage('Checkout Stage') {
+                steps{
+                    script{
+                        echo "${params.Environment}"
+                        echo "${params.GithubRepo}"
+                        // def release_branch = "${params.Branch}"
+                        // def release = release_branch.substring(7)
+                        git branch: 'develop', url: "${params.GithubRepo}"
+                        sh "git checkout develop"
+                    }
+                }
+            }
+    }
+}
